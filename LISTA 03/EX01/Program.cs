f668c60730af18f67e0928a24eedb918c2975af7
Noemi Santos de Oliveira <osantosnoemi@gmail.com>
2025-09-23 17:48:10 -0300

    ex04

---
+++ LISTA 03/EX01/Program.cs
@@ -0,0 +1,14 @@
+﻿//Variavel
+int soma = 0;
+
+//Processamento
+//Se for múltiplo de 3 e ímpar, será somado
+for (int x = 1; x <= 500; x++)
+{
+    if (x % 2 != 0 && x % 3 == 0)
+    {
+        soma = soma + x;
+    }
+}
+//Sáiida de dados
+Console.WriteLine("A soma dos números ímpares e múltiplos de 3 é: " + soma);
---
+++ LISTA 03/EX03/Program.cs
@@ -0,0 +1,13 @@
+﻿//Variaveis
+int a, b, s, m;
+
+//Processando dados
+for (int x = 1000; x <= 9999; x++)
+{
+    a = x / 100; //pega os dois primeiros digitos
+    b = x % 100; //pega os dois últimos digitos
+    s = a + b; //soma os dois
+    m = s * s; //multiplica o resultado da soma por si mesma
+    if (m == x) //compara o resulta com o valor atual de x
+        Console.WriteLine("Resultado: " + m); //saída de dados
+}
