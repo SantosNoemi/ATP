f7d167194f5f7c6e033f43fdfe21f396bffe9d04
Noemi Santos de Oliveira <osantosnoemi@gmail.com>
2025-09-27 18:19:33 -0300

    EX05 e EX06

---
+++ LISTA 03/EX05/Program.cs
@@ -0,0 +1,22 @@
+﻿//declara PI com valor inicial 0, pois o valor sera atualizado
+double pi = 0; 
+
+//Usuário informa a quantidade de termos
+Console.WriteLine ("Informe quantos termos serão calculados na serie de Leibniz: ");
+int n = int.Parse (Console.ReadLine());
+
+//laço de repetição para realizar o cálculo com a quantidade de termos informada
+for (int i = 0; i < n; i++)
+{
+    //Math.Pow (-1... é para sempre alternar entre + ou -
+    //sempre que i for par, o sinal é +
+    //sempre que i for impar, o sinal é -
+    double termo = Math.Pow (-1, i) / (2 * i +1); 
+    //Atualiza o valor de PI e deposita na variável
+    pi = pi + termo;
+}
+//depois multiplica o resultado por 4 para se apróximar do valor de PI
+pi = pi * 4;
+
+//saída de dados
+Console.WriteLine ($"O valor aproximado de PI com {n} termos é: {pi}");
---
+++ LISTA 03/EX06/Program.cs
@@ -0,0 +1,24 @@
+﻿//Variaveis
+int a, n, soma = 0;
+
+//Pede ao usuario para indicar o valor de A e N
+Console.WriteLine("Informe o valor de A: ");
+a = int.Parse (Console.ReadLine());
+Console.WriteLine("Informe o valor de N: ");
+n = int.Parse (Console.ReadLine());
+
+//Condicional para caso o A fornecido seja negativo
+while (n <= 0)
+{
+    Console.WriteLine("Número inválido. Informe um novo valor para N: ");
+    n = int.Parse (Console.ReadLine());
+}
+//Laço de repetição para atualizar o valor de soma e somar com o valor de A
+//Dessa forma, os próximos números após A estão na sequencia numerica
+//Ex: soma = 0, A = 3, i = 1 | soma = 0 + 3 + 1 | soma = 4
+for (int i = 0; i < n; i++)
+{
+    soma = soma + a + i;
+} 
+//Saída de dados
+Console.WriteLine ("A soma dos {0} termos à partir de A foram: {1}", n, soma);
