991feec429759a12f1cbf23453f275ddb6e84ddc
Noemi Santos de Oliveira <osantosnoemi@gmail.com>
2025-09-27 18:11:51 -0300

    inicio lista 3

---
+++ LISTA 03/EX04/Program.cs
@@ -0,0 +1,23 @@
+﻿//Variáveis
+int a = 0, b = 1, c, n;
+
+//Entrada
+Console.WriteLine("Digite o número de termos da sequência de Fibonacci que deseja exibir:");
+n = int.Parse(Console.ReadLine());
+
+//Processamento e Saída
+if (n <= 0)
+{
+    Console.WriteLine("Digite um número maior que zero.");
+}
+else
+{
+    Console.WriteLine("Sequencia de Fibonacci:");
+    for (int i = 1; i <= n; i++) //Loop para calcular e exibir os termos
+    {
+        Console.Write(a + " "); //Exibe o valor atual de 'a'
+        c = a + b; //Calcula o próximo termo   
+        a = b;
+        b = c;
+    }
+}
